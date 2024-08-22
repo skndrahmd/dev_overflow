4ea7aa6389b2c2aced42d4a7de90687933063402
Sikander Ahmed <sa07421@st.habib.edu.pk>
2024-08-22 08:55:57 +0500

    added auth

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,19 +1,52 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
 const Navbar = () => {
   return (
-    <div>
-        <Link href="/">
+    <div className="bg-light-800 dark:bg-dark-500 shadow-light-200 dark:shadow-dark-100 p-4">
+      <nav className="container mx-auto flex justify-between items-center">
+        <Link
+          href="/"
+          className="text-primary-500 font-spaceGrotesk text-lg hover:text-accent-blue transition duration-300"
+        >
           Home
         </Link>
-        <Link href="/about">
-            About
+        <Link
+          href="/about"
+          className="text-primary-500 font-spaceGrotesk text-lg hover:text-accent-blue transition duration-300"
+        >
+          About
         </Link>
-        <Link href="/contact">
-           Contact
+        <Link
+          href="/contact"
+          className="text-primary-500 font-spaceGrotesk text-lg hover:text-accent-blue transition duration-300"
+        >
+          Contact
         </Link>
+        <Link
+          href="/ask-question"
+          className="text-primary-500 font-spaceGrotesk text-lg hover:text-accent-blue transition duration-300"
+        >
+          Ask Question
+        </Link>
+        <ClerkProvider>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </ClerkProvider>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
